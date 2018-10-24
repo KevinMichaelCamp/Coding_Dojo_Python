8b72713ee6ca4cb6585a7010cc9faa12382f6ae2
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-24 17:10:41 -0500

    Functions_Basic2

--- Python_Fundamentals/For_Loop_Basic2.py
+++ Python_Fundamentals/For_Loop_Basic2.py
@@ -1,3 +1,19 @@
 # For Loop Basic 2
 
-## 1. - Biggie Size - 
+# 1. Biggie Size - Gievn an array, write a function that changes all positive numbers in the array to "big". Example: makeItBig([-1,3,5,-5]) returns that same array. changed to [-1,"big","big",-5].
+
+# 2. Count Positives - Given an array of numbers, create a function to replace last value with number of positive valuse. Example, countPositives([-1,1,1,1]) changes the array to [-1,1,1,3] and returns it. (Note that zero is not considered to be a positive number).
+
+# 3. SumTotal - Create a function that takes an array as an argument and returns the sum of all the values in the array. For example sumTotal([1,2,3,4]) should return 10.
+
+# 4. Average - Create a function that takes an array as an argument and returns the average of all values in the array. For example average([1,2,3,4]) should return 2.5.
+
+# 5. Length - Create a function that takes an array as an argument and returns the length of the array. For example length([1,2,3,4]) should return 4.
+
+# 6. Minimum - Create a function that takes an array as an argument and returns the minimum value in the array. If the passed array is empty, have the function return False. For example, minimum([1,2,3,4]) should return 1; minimum([-1,-2,-3]) should return -3.
+
+# 7. Maximum - Create a function that takes an array as an argument and returns the maximum value in the array. If the passed array is empty, have the function return False. For example, maximum([1,2,3,4]) should return 4; maximum([-1,-2,-3]) should return -1.
+
+# 8. UltimateAnalyze - Create a function that takes an array as an argument and returns a dictionary that has the sumTotal, average, minimum, maximum, and length of the aray.
+
+# 9. ReverseList - Create a function that takes an array as an argument and return an array in a reversed order. Do this without creating an empty temporary array. For example reverse([1,2,3,4]) should return [4,3,2,1]. This challenge is known to appear during basic technical interviews.
--- Python_Fundamentals/Functions_Basic2.py
+++ Python_Fundamentals/Functions_Basic2.py
@@ -1,15 +1,15 @@
 # Functions Basic 2
 
-## 1. - Countdown - Create a function that accepts a number as an inout. Return a new array that counts down by one, from the number (as array's 'zero'th element) down to 0 (as the last element). For example countDown(5) should return [5,4,3,2,1,0]
+## 1. - Countdown - Create a function that accepts a number as an inout. Return a new array that counts down by one, from the number (as array's 'zero'th element) down to 0 (as the last element). For example countDown(5) should return [5,4,3,2,1,0].
 
 
-## 2. - Print & Return - Your function will receive an array with two numbers. Print the first value, and return the second.abs
+## 2. - Print & Return - Your function will receive an array with two numbers. Print the first value, and return the second.
 
 
-## 3. - First Plus Length - Given an array, return the sum of the first value in the array, plus the array's lengthself.
+## 3. - First Plus Length - Given an array, return the sum of the first value in the array, plus the array's length.
 
 
 ## 4. - Valuse Greater Than Second - Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is. If the array is only one element long, have the function return Falseself.
 
 
-## 5. - This Length, That Value - Write a function called lengthAndValue which accepts two parameters, size and value. This function should take two numbers and return a list of length size containing only the number in value. For example, lengthAndValue(4,7) should return [7,7,7,7]. 
+## 5. - This Length, That Value - Write a function called lengthAndValue which accepts two parameters, size and value. This function should take two numbers and return a list of length size containing only the number in value. For example, lengthAndValue(4,7) should return [7,7,7,7].

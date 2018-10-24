87933ff64436b0b1dc76742dc8cddc59e057d760
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-24 17:12:14 -0500

    For_Loop_Basic2

--- Python_Fundamentals/For_Loop_Basic2.py
+++ Python_Fundamentals/For_Loop_Basic2.py
@@ -1,8 +1,8 @@
 # For Loop Basic 2
 
-# 1. Biggie Size - Gievn an array, write a function that changes all positive numbers in the array to "big". Example: makeItBig([-1,3,5,-5]) returns that same array. changed to [-1,"big","big",-5].
+# 1. Biggie Size - Given an array, write a function that changes all positive numbers in the array to "big". Example: makeItBig([-1,3,5,-5]) returns that same array. changed to [-1,"big","big",-5].
 
-# 2. Count Positives - Given an array of numbers, create a function to replace last value with number of positive valuse. Example, countPositives([-1,1,1,1]) changes the array to [-1,1,1,3] and returns it. (Note that zero is not considered to be a positive number).
+# 2. Count Positives - Given an array of numbers, create a function to replace last value with the number of positive values. Example, countPositives([-1,1,1,1]) changes the array to [-1,1,1,3] and returns it. (Note that zero is not considered to be a positive number).
 
 # 3. SumTotal - Create a function that takes an array as an argument and returns the sum of all the values in the array. For example sumTotal([1,2,3,4]) should return 10.
 

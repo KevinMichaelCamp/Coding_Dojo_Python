0c96e70ffc1040e4770429bec9c9b2829dba3d2f
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-24 17:11:23 -0500

    Functions_Basic2

--- Python_Fundamentals/Functions_Basic2.py
+++ Python_Fundamentals/Functions_Basic2.py
@@ -9,7 +9,7 @@
 ## 3. - First Plus Length - Given an array, return the sum of the first value in the array, plus the array's length.
 
 
-## 4. - Valuse Greater Than Second - Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is. If the array is only one element long, have the function return Falseself.
+## 4. - Values Greater Than Second - Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is. If the array is only one element long, have the function return False.
 
 
 ## 5. - This Length, That Value - Write a function called lengthAndValue which accepts two parameters, size and value. This function should take two numbers and return a list of length size containing only the number in value. For example, lengthAndValue(4,7) should return [7,7,7,7].

cf79f0e58bb58007ffec64b6b867013b749fe5f3
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-25 11:35:25 -0500

    For_Loop_Basic1

--- Python_Fundamentals/For_Loop_Basic1.py
+++ Python_Fundamentals/For_Loop_Basic1.py
@@ -1,11 +1,11 @@
-#For Loop Basic 1
+# For Loop Basic 1
 
 # 1. Basic - Print all the numbers/integers from 0 to 150
 for num in range(0, 151):
     print(num)
 
 # 2. Multiples of five - Print all the multiples of 5 from 5 to 1,000,000
-for five in range(0,100001):
+for five in range(0, 100001):
     if(five % 5 == 0):
         print(five)
 
@@ -36,14 +36,15 @@ for num in range(2018, 0, -1):
         print(num)
 
 # 6. Flexible Countdown - Given 'lolwNum', 'highNum', 'mult', print multiples of mult from lowNum to highNum, using a FOR loop. For (2,9,3), print 3 6 9 (on succesive lines)
+
+
 def multiples(lowNum, highNum, mult):
     for num in range(lowNum, highNum):
         if(num % mult == 0):
             print(num)
 
 
-
 # Test Case
-multiples(0,100,2)
-multiples(0,100,3)
-multiples(0,100,4)
+multiples(0, 100, 2)
+multiples(0, 100, 3)
+multiples(0, 100, 4)

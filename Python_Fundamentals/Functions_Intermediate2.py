1814e1c873d1501513b333247fb14615dbf0553a
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-11-07 08:37:04 -0600

    Intermediate2

--- Python_Fundamentals/Functions_Intermediate2.py
+++ Python_Fundamentals/Functions_Intermediate2.py
@@ -1,1 +1,27 @@
 # Functions Intermediate 2
+
+# Problem 1
+
+x = [ [5,2,3], [10,8,9] ]
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+
+z = [ {'x': 10, 'y': 20} ]
+
+# How would you change the value 10 in x to 15?  Once you're done x should then be [ [5,2,3], [15,8,9] ].
+
+# How would you change the last_name of the first student from 'Jordan' to "Bryant"?
+
+# For the sports_directory, how would you change 'Messi' to 'Andres'?
+
+# For z, how would you change the value 20 to 30?
+
+# Problem 2

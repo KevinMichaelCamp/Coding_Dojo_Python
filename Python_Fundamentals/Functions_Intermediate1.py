39ec5a6927b3a215dc8ea31411ee0cd4719fad13
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-11-06 21:04:22 -0600

    Intermediate 1

--- Python_Fundamentals/Functions_Intermediate1.py
+++ Python_Fundamentals/Functions_Intermediate1.py
@@ -1,1 +1,14 @@
 # Functions Intermediate 1
+
+import random
+
+def randInt(max=100):
+    print(int(random.random() * max))
+
+randInt()
+randInt(max=50)
+
+def randInt(min):
+    print(int(random.random() * min) + min)
+
+randInt(min=50)

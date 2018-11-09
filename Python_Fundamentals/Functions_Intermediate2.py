3427b70f95d4138e29246a41110590026681ebed
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-11-08 22:04:02 -0600

    PyChanges

--- Python_Fundamentals/Functions_Intermediate2.py
+++ Python_Fundamentals/Functions_Intermediate2.py
@@ -18,6 +18,9 @@ z = [ {'x': 10, 'y': 20} ]
 
 # How would you change the value 10 in x to 15?  Once you're done x should then be [ [5,2,3], [15,8,9] ].
 
+x[1] = [15,8,9]
+print(x)
+
 # How would you change the last_name of the first student from 'Jordan' to "Bryant"?
 
 # For the sports_directory, how would you change 'Messi' to 'Andres'?

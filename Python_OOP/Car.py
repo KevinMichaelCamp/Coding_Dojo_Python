ede44fa841510a35095cb119474d3cc21f87137b
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-11-20 14:04:54 -0600

    CAR.PY

--- Python_OOP/Car.py
+++ Python_OOP/Car.py
@@ -1,1 +1,26 @@
 # Car Assignment
+
+
+class Car:
+    def __init__(self, price, speed, fuel, mileage):
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        if price > 10000:
+            self.tax = "15%"
+        else:
+            self.tax = "12%"
+
+    def displayAll(self):
+        print("Price: " + str(self.price))
+        print("Speed: " + self.speed)
+        print("Fuel: " + self.fuel)
+        print("Mileage: " + self.mileage)
+        print("Tax: " + self.tax)
+
+
+car1 = Car(15000, '120mph', 'Full', '45mpg')
+car1.displayAll()
+car1 = Car(7500, '100mph', 'Empty', '25mpg')
+car1.displayAll()

6fdfee13312240b2c4c285aedcf9a9e7bf3ada66
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-12-20 17:12:43 -0600

    Hello_Flask

--- Flask_Fundamentals/hello_flask/hello.py
+++ Flask_Fundamentals/hello_flask/hello.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 print(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html', name="Kevin")
+
+@app.route('/hello_world')
+def hello_world():
     return 'Hello World!'
 
 @app.route('/coding')

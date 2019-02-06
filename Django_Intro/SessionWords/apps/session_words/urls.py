a3a8a27f74f9fcf59b8b8f9b05b895b5eab02856
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-02-06 12:29:57 -0600

    Session Words

--- Django_Intro/SessionWords/apps/session_words/urls.py
+++ Django_Intro/SessionWords/apps/session_words/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^create$', views.create, name='create'),
+    url(r'^clear$', views.clear, name='clear'),
 ]
--- Django_Intro/SessionWords/apps/session_words/views.py
+++ Django_Intro/SessionWords/apps/session_words/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render, redirect
+from time import localtime, strftime
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if not 'words' in request.session:
+        request.session['words'] = []
+
+    if 'size' in request.POST:
+        font = '200%'
+    else:
+        font = '100%'
+
+    if 'color' in request.POST:
+        color = request.POST['color']
+    else:
+        color = 'black'
+
+    all_words = request.session['words']
+    if request.method == 'POST':
+        all_words.append({'word': request.POST['words'], 'color': color, 'font': font, 'time': strftime(
+            "%b-%d-%Y %H:%M %p", localtime())})
+
+    print(all_words)
+    request.session['words'] = all_words
+    return redirect('/')
+
+
+def clear(request):
+    request.session.clear()
+    return redirect('/')

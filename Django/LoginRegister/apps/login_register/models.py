c2f79ffe8d57ca14e5d4ba971843e8fc0f379278
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-02-18 21:21:54 -0600

    Login Register

---
+++ Django/LoginRegister/apps/login_register/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+import bcrypt
+import re
+
+# Create your models here.
+EMAIL_REGEX = re.compile(r'[a-zA-Z0-9.+_-]+@[a-zA_Z0-9._-]+\.[a-zA-z]+$')
+NAME_REGEX = re.compile(r'[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First Name must be at least 2 characters"
+        elif not NAME_REGEX.match(postData['first_name']):
+            errors['first_name'] = "First name must only contain letters"
+
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last Name must be at least 2 characters"
+        elif not NAME_REGEX.match(postData['first_name']):
+            errors['last_name'] = "Last name must only contain letters"
+
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email format"
+        elif len(User.objects.filter(email=postData['email'])) > 0:
+            errors['email'] = "Email already registered"
+
+        if len(postData['password']) < 8:
+            errors['password'] = "Password must be at least 8 characters"
+
+        if postData['password'] != postData['confirm']:
+            errors['password'] = "Password must match password confirmation"
+        return errors
+
+    def validate_login(self, postData):
+        errors = {}
+        if len(User.objects.filter(email = postData['email'])):
+            user = User.objects.get(email = postData['email'])
+            if bcrypt.checkpw(postData['password'].encode(), user.password.encode()):
+                return user
+            else:
+                errors['login'] = "Email/password incorrect"
+                return errors
+        else:
+            errors['login'] = "Email/password incorrect"
+            return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    confirm = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return "<User object: {} {} {}".format(self.first_name, self.last_name, self.email)
+
+    objects = UserManager()
---
+++ Django/LoginRegister/apps/login_register/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import User
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    errors = User.objects.validate(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+
+    else:
+        User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+            )
+        messages.success(request, "New User Registered")
+        return redirect('/success')
+
+def login(request):
+    errors = User.objects.validate_login(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+
+    else:
+        messages.success(request, "User Logged In")
+        return redirect('/success')
+
+def success(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'success.html', context)
